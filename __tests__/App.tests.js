6aef92f110edfd023dd87762c6e69c0ba6a8043a
cass1618 <cass16180@gmail.com>
2021-06-21 13:42:02 -0700

    Fix commit messages to include coauthors

--- __tests__/App.tests.js
+++ __tests__/App.tests.js
@@ -1,5 +1,7 @@
 import calculateChange from './../CoinCounter.js';
 
 describe('calculateChange', () => {
-
+TextDecoderStream('it should return the amount of quarters, dimes, nickels, and pennies into a string', () => {
+  
+})
 })
